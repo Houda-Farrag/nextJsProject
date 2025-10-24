bc0a107fb4b82ee9b0c86eae6a468bd37cb7d99a
mahmoud farrag <mahmiti2022@gmail.com>
2025-10-25 02:14:20 +0300

    updating

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import { ReactNode } from "react";
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 type SidebarLink = {
   href: string;
   label: string;
